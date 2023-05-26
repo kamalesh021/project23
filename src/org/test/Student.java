dbfa4a097b0bf1d7f091c3710a405f3e96c7797b
kamalesh <kamaleshkumar@02126>
2023-05-26 11:39:28 +0530

    sample

---
+++ src/org/test/Student.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class Student {
+	private void studentMark() {
+		System.out.println("student mark is 81  ");
+		
+		// TODO Auto-generated method stub
+
+	}
+	private void studentId() {
+		System.out.println("student Id is 12345");
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.studentMark();
+		s.studentId();
+	}
+
+}
